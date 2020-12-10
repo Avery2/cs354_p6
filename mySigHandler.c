e4242e20f5a4d54f30b5c760ebf5bcf603b47e58
Avery2 <justinaverychan@gmail.com>
2020-12-09 23:25:03 -0600

    meh

--- mySigHandler.c
+++ mySigHandler.c
@@ -11,7 +11,9 @@ static void alarm_handler()
 	printf("handler called\n");
 	// handler function
 	// print the pid (process id) and current time (format linux date command)
-	printf("%d %s", getpid(), ctime(time(NULL)));
+	time_t now;
+	time(&now);
+	printf("%d %s", getpid(), ctime(time(&now)));
 	printf("reset alarm\n");
 	alarm(DELAY);
 	return;
