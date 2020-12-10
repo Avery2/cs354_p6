716b8cfb243464ec3fa1f017ed608d5344f841c3
Avery2 <justinaverychan@gmail.com>
2020-12-09 23:17:49 -0600

    meh

--- mySigHandler.c
+++ mySigHandler.c
@@ -17,13 +17,14 @@ static void alarm_handler()
 
 int main(int argc, char *argv[])
 {
-	printf("Pid and time print every 3 seconds.\nEnter Ctrl-C to end the program.");
+	printf("Pid and time print every 3 seconds.\nEnter Ctrl-C to end the program.\n");
 
 	alarm(DELAY); // setup alarm to go off 3 seconds later, sending SIGALRM signal to the program
 	// register signal handler to handle the SIGALRM (14)
 	struct sigaction sa;
+
 	sa.sa_flags = SA_SIGINFO;
-	sigemptyset(&sa.sa_mask);
+	// sigemptyset(&sa.sa_mask);
 	sa.sa_sigaction = alarm_handler;
 	sigaction(SIGALRM, &sa, NULL);
 	while (1)
