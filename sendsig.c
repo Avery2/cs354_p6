0fa8852ea55468fd7e9547116dd958e6054d7a76
Avery2 <justinaverychan@gmail.com>
2020-12-10 15:37:55 -0600

    started sendsig

--- sendsig.c
+++ sendsig.c
@@ -1,17 +1,37 @@
 #include <signal.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define SIGUSR1 10
+#define SIGINT 2
 
 int main(int argc, char *argv[])
 {
     if (argc != 3)
     {
         printf("Usage: <signal type> <pid>\n");
+        exit(0);
     }
-    else
+    if (!(strcmp(argv[1], "-u") == 0 || strcmp(argv[1], "-i") == 0))
     {
-        for (int i = 0; i < argc; i++)
-        {
-            printf("%s\n", argv[i]);
-        }
+        // TODO do I keep this? Or remove extra testing
+        printf("Usage: <signal type> <pid>\n");
+    }
+    for (int i = 0; i < argc; i++)
+    {
+        printf("%s\n", argv[i]);
+    }
+
+    // check error atoi
+    pid_t pid = atoi(argv[2]);
+
+    if (strcmp(argv[1], "-u") == 0)
+    {
+        kill(pid, SIGUSR1);
+    }
+    if (strcmp(argv[1], "-i") == 0)
+    {
+        kill(pid, SIGINT);
     }
 }
