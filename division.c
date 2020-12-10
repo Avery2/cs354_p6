ccc7862401a5bf7785950c384ec7c6eae428db4a
Avery2 <justinaverychan@gmail.com>
2020-12-10 16:10:03 -0600

    division skeleton

--- division.c
+++ division.c
@@ -3,8 +3,32 @@
 
 #define SIGFPE 8
 
+int div_count = 0;
+
+static void interrupt_handler()
+{
+    printf("Total number of operations completed successfully: %d\nThe program will be terminated.\n", div_count);
+}
+
+static void div_zero_hanlder()
+{
+    printf("Error: a division by 0 operation was attempted.\nTotal number of operations completed successfully: %d\nThe program will be terminated.\n", div_count);
+}
+
 int main(int argc, char *argv[])
 {
+    struct sigaction div_zero_action;
+    memset(&div_zero_action, 0, sizeof(div_zero_action));
+    div_zero_action.sa_flags = SA_SIGINFO;
+    div_zero_action.sa_sigaction = div_zero_hanlder;
+    sigaction(SIGALRM, &div_zero_action, NULL);
+
+    struct sigaction interrupt_action;
+    memset(&interrupt_action, 0, sizeof(interrupt_action));
+    interrupt_action.sa_flags = SA_SIGINFO;
+    interrupt_action.sa_sigaction = interrupt_handler;
+    sigaction(SIGALRM, &interrupt_action, NULL);
+
     while (1)
     {
         // non-numeric input defaults to 0
@@ -17,5 +41,6 @@ int main(int argc, char *argv[])
         int n1 = atoi(input1);
         int n2 = atoi(input2);
         printf("%d / %d is %d with a remainder of %d\n", n1, n2, n1 / n2, n1 % n2);
+        div_count++;
     }
 }
