4bc857db36f1010d8beb43af501b50603e878968
Avery2 <justinaverychan@gmail.com>
2020-12-09 22:59:29 -0600

    meh

--- mySigHandler.c
+++ mySigHandler.c
@@ -4,22 +4,22 @@
 #include <time.h>
 
 #define SIGALRM 14
+#define DELAY 3
 
 void alarm_handler()
 {
+	// handler function
+	// print the pid (process id) and current time (format linux date command)
 	printf("%d %ld", getpid(), time(NULL));
-	alarm(3);
+	alarm(DELAY);
 	return;
 }
 
 int main()
 {
-	printf("START");
-	alarm(3); // setup alarm to go off 3 seconds later, sending SIGALRM signal to the program
+	printf("Pid and time print every 3 seconds.\nEnter Ctrl-C to end the program.");
+	alarm(DELAY); // setup alarm to go off 3 seconds later, sending SIGALRM signal to the program
 	// register signal handler to handle the SIGALRM (14)
-	// handler function
-	// print the pid (process id) and current time (format linux date command)
-
 	signal(SIGALRM, alarm_handler);
 	while (1)
 	{
