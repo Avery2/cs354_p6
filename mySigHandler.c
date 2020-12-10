68a799f1cf04257b148b55bdfd04953ec2154708
Avery2 <justinaverychan@gmail.com>
2020-12-09 22:42:17 -0600

    first try at first part

--- mySigHandler.c
+++ mySigHandler.c
@@ -1,6 +1,23 @@
+#include <unistd.h>
 
-main() {
-	while(1) {
+#define SIGALRM 14
 
+main()
+{
+	alarm(3); // setup alarm to go off 3 seconds later, sending SIGALRM signal to the program
+	// register signal handler to handle the SIGALRM (14)
+	// handler function
+	// print the pid (process id) and current time (format linux date command)
+
+	signal(SIGALRM, alarm_handler);
+
+	while (1)
+	{
 	}
+}
+
+alarm_handler()
+{
+	print(sys_getpid() + sys_time());
+	alarm(3);
 }
