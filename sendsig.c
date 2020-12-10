5786f1a0417ea18ffb8ff4d95ab3ecc6cda5741b
Avery2 <justinaverychan@gmail.com>
2020-12-10 17:21:36 -0600

    added headers

--- sendsig.c
+++ sendsig.c
@@ -1,3 +1,35 @@
+////////////////////////////////////////////////////////////////////////////////
+// Main File:        sendsig.c
+// This File:        sendsig.c
+// Other Files:      mySigHandler.c division.c
+// Semester:         CS 354 Fall 2020
+// Instructor:       deppeler
+//
+// Discussion Group: 631
+// Author:           Justin Chan
+// Email:            jachan@wisc.edu
+// CS Login:         justinc
+//
+/////////////////////////// OTHER SOURCES OF HELP //////////////////////////////
+//                   fully acknowledge and credit all sources of help,
+//                   other than Instructors and TAs.
+//
+// Persons:          Identify persons by name, relationship to you, and email.
+//                   Describe in detail the the ideas and help they provided.
+//
+// Online sources:   avoid web searches to solve your problems, but if you do
+//                   search, be sure to include Web URLs and description of
+//                   of any information you find.
+//////////////////////////// 80 columns wide ///////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////////
+//
+// Copyright 2013,2019-2020, Jim Skrentny, (skrentny@cs.wisc.edu)
+// Posting or sharing this file is prohibited, including any changes/additions.
+// Used by permission, CS354-Fall 2020, Deb Deppeler (deppeler@cs.wisc.edu)
+//
+////////////////////////////////////////////////////////////////////////////////
+
 #include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
